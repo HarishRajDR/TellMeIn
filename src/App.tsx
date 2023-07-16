1811f0bf26eb14da11ecbce46a3f835c06f594c8
Harish Raj D R <drharishrajdr@gmail.com>
2023-07-16 11:31:33 +0530

    Made basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 function App() {
   return (
-    <div className="flex justify-center">
-      <h1>Hello There!</h1>
+    <div className="flex justify-center p-20 flex-col items-center gap-20">
+      <h1 className="text-7xl">How Much in</h1>
+      <form className="flex gap-2">
+        <input
+          type="number"
+          min={0}
+          step={"any"}
+          placeholder="Enter your measurement"
+          className="px-5 py-6 text-3xl outline-indigo-600 border-solid rounded-md "
+        />
+        <button className="rounded-md bg-indigo-600 px-5 py-6 text-3xl font-semibold text-white ">
+          Go!
+        </button>
+      </form>
     </div>
   );
 }
