c420de0ba370f6aeb80461fdebc53eae9ed878e5
Harish Raj D R <drharishrajdr@gmail.com>
2023-07-16 13:38:24 +0530

    Added loading state

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 const Result = () => {
-  return <div>Result</div>;
+  const [isloading, setloading] = useState(true);
+
+  setTimeout(() => {
+    setloading(false);
+  }, 2000);
+
+  if (isloading) {
+    return <h2>Loading...</h2>;
+  } else {
+    return <h2>Done Loading</h2>;
+  }
 };
 
 export default Result;
