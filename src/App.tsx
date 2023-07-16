c9969793fd600ee97b775fe291d8a80fcca55c21
Harish Raj D R <drharishrajdr@gmail.com>
2023-07-16 12:49:27 +0530

    Added DropDown and tweaked the styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import DropDown from "./components/DropDown";
+
 function App() {
   return (
     <div className="flex justify-center p-20 flex-col items-center gap-20">
@@ -8,9 +10,12 @@ function App() {
           min={0}
           step={"any"}
           placeholder="Enter your measurement"
-          className="px-5 py-6 text-3xl outline-indigo-600 border-solid rounded-md "
+          className=" w-full text-2xl outline-indigo-600 rounded-md py-2 pl-5 pr-10 mt-1"
         />
-        <button className="rounded-md bg-indigo-600 px-5 py-6 text-3xl font-semibold text-white ">
+
+        <DropDown />
+
+        <button className="rounded-md bg-indigo-600 text-2xl font-semibold text-white text-center py-4 px-10 mt-1 ">
           Go!
         </button>
       </form>
