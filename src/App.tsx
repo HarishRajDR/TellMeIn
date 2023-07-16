092d5509be92387c57353ef66564539db6f0963c
Harish Raj D R <drharishrajdr@gmail.com>
2023-07-16 13:30:54 +0530

    moved the props into App.js and added some types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import DropDown from "./components/DropDown";
+import Result from "./components/Result";
+
+const units = [
+  "Kilometre",
+  "Meter",
+  "Centimeter",
+  "Millimetre",
+  "Micrometres",
+  "Nanometre",
+  "Mile",
+  "Yard",
+  "Foot",
+  "Inch",
+  "Nautical mile",
+];
 
 function App() {
+  const [selectedUnit, setSelectedUnit] = useState(units[0]);
+
   return (
     <div className="flex justify-center p-20 flex-col items-center gap-20">
-      <h1 className="text-7xl">How Much in</h1>
+      <h1 className="text-7xl font-bold">How Much in</h1>
       <form className="flex gap-2">
         <input
           type="number"
@@ -13,12 +31,14 @@ function App() {
           className=" w-full text-2xl outline-indigo-600 rounded-md py-2 pl-5 pr-10 mt-1"
         />
 
-        <DropDown />
+        <DropDown list={units} unit={selectedUnit} funct={setSelectedUnit} />
 
         <button className="rounded-md bg-indigo-600 text-2xl font-semibold text-white text-center py-4 px-10 mt-1 ">
           Go!
         </button>
       </form>
+
+      <Result />
     </div>
   );
 }
--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,18 +1,21 @@
 import { Listbox, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
-import { Fragment, useState } from "react";
 
-const units = ["Centimeter", "Meter", "Kilometer"];
+type Props = {
+  list: Array<string>;
+  unit: string;
+  funct: any;
+};
 
-function DropDown() {
-  const [selectedUnit, setSelectedUnit] = useState(units[0]);
-
+function DropDown(props: Props) {
   return (
-    <div className="w-96 ">
-      <Listbox value={selectedUnit} onChange={setSelectedUnit}>
+    <div className="w-[30rem]">
+      <Listbox value={props.unit} onChange={props.funct}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-[#3b3b3b] py-4 pl-5 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-2xl">
-            <span className="block truncate">{selectedUnit}</span>
+            <span className="block truncate">{props.unit}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
                 className="h-5 w-5 text-gray-400"
@@ -27,7 +30,7 @@ function DropDown() {
             leaveTo="opacity-0"
           >
             <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-[#3b3b3b] py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-2xl">
-              {units.map((p) => (
+              {props.list.map((p) => (
                 <Listbox.Option
                   key={p}
                   value={p}
